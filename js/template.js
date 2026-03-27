a9416e9e771bcb4ae6aa5db03b37ed71ccf8c257
u7663394 <u7663394@anu.edu.au>
2026-03-27 15:59:54 +1100

    feat: add encoded AprilTag to template page

--- js/template.js
+++ js/template.js
@@ -17,25 +17,4 @@ function setRootVariables({ PW, PH, PTW, PTH, al, TH }) {
   root.style.setProperty("--th", `${TH}mm`);
 }
 
-function updateInfo({ PW, PH, PTW, PTH, al, TH }) {
-  const leftMargin = (PW - PTW) / 2;
-  const rightMargin = (PW - PTW) / 2;
-  const topMargin = 0.2 * al;
-  const patternTop = al + TH;
-  const bottomMargin = PH - patternTop - PTH;
-
-  document.getElementById("info").innerHTML = `
-          <div><strong>Paper</strong>: ${PW}mm × ${PH}mm</div>
-          <div><strong>Pattern</strong>: ${PTW}mm × ${PTH}mm</div>
-          <div><strong>AprilTag</strong>: ${al}mm</div>
-          <div><strong>Text area height</strong>: ${TH}mm</div>
-          <div><strong>Left margin</strong>: ${leftMargin.toFixed(2)}mm</div>
-          <div><strong>Right margin</strong>: ${rightMargin.toFixed(2)}mm</div>
-          <div><strong>Top margin</strong>: ${topMargin.toFixed(2)}mm</div>
-          <div><strong>Pattern top</strong>: ${patternTop.toFixed(2)}mm</div>
-          <div><strong>Bottom margin</strong>: ${bottomMargin.toFixed(2)}mm</div>
-        `;
-}
-
 setRootVariables(params);
-updateInfo(params);
